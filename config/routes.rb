b0832b25d292e5791e6a89c0950cc7c6101bee14
okots <kots.olesya@gmail.com>
2017-04-19 17:32:59 +0300

    g post

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :posts
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+  resource :posts
+  root to: "home#show"
+
+end
