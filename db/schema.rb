45d7c0ca15b7b1b62298268bdaf1940743aa4f46
Kots Olesya <kots.olesya@gmail.com>
2017-04-19 20:20:00 +0300

    postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170419142245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "url"
     t.datetime "created_at", null: false
